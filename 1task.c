f47188f249d495ff9de3536beb1463da3660ef61
Paul Kotsko <118366151+Pau1Net@users.noreply.github.com>
2023-03-10 21:16:58 +0300

    Update 1task.c

--- 1task.c
+++ 1task.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    fprintf(stdout, "Student group: A591\n"); // stdout is the standard output stream
+    fprintf(stdout, "Student group: A591\n");
     fprintf(stdout, "Name of Student: Pavel Kotsko\n");
     return 0;
 }
