2002a026041a749a2bd88a0b0393b77056f4740d
Paul Kotsko <118366151+Pau1Net@users.noreply.github.com>
2023-03-10 21:19:09 +0300

    Update 1task.c

--- 1task.c
+++ 1task.c
@@ -5,3 +5,8 @@ int main() {
     fprintf(stdout, "Name of Student: Pavel Kotsko\n");
     return 0;
 }
+/*
+Команды в терминале:
+1) gcc -o A591_KotskoPA_1task 1lrsrv.c
+2) /root/A591_KotskoPA_1task
+*/
