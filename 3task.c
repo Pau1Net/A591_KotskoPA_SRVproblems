52e11e0dd3edc6e8827feb1b84f1e2ab093c2db9
Paul Kotsko <118366151+Pau1Net@users.noreply.github.com>
2023-04-08 20:42:28 +0300

    Update 3task.c

--- 3task.c
+++ 3task.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <sys/neutrino.h>
 
+//структура сообщения для сервера
 typedef struct {
     char text[256];
 } message_t;
@@ -15,27 +16,31 @@ void server(void) {
     message_t msg;
     message_t reply;
 
-    // Create a channel
+    //создание канала
     chid = ChannelCreate(0);
     if (chid == -1) {
-        perror("Ошибка - ChannelCreate ");
+        perror("ChannelCreate");
         exit(EXIT_FAILURE);
     }
 
-    printf("Поехали, ID канала: %d\n", chid);
+    printf("Server started, channel ID: %d\n", chid);
 
+    //ожидание сообщений и ответ на них в бесконечном цикле
     for (;;) {
+        //получение сообщения
         rcvid = MsgReceive(chid, &msg, sizeof(msg), NULL);
         if (rcvid == -1) {
             perror("MsgReceive");
             continue;
         }
 
-        printf("Полученное сообщение: %s\n", msg.text);
+        printf("Received message: %s\n", msg.text);
 
+        //обработка сообщения и создание ответа        
         strncpy(reply.text, msg.text, sizeof(reply.text) - 1);
         reply.text[sizeof(reply.text) - 1] = '\0';
 
+        //Ответ на сообщение
         if (MsgReply(rcvid, EOK, &reply, sizeof(reply)) == -1) {
             perror("MsgReply");
         } else {
@@ -43,7 +48,7 @@ void server(void) {
         }
     }
 }
-
+//запуск
 int main(void) {
     server();
     return 0;
